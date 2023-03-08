eae798ad87d08a67abf4b783e583b982da0e2d8c
erazmozen <erazmozen@protonmail.com>
2023-03-08 14:18:30 +0100

    Render sorted games on summary

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -3,7 +3,21 @@ const Summary = ({ games }) => {
     (a, b) => b.gameTotalScore - a.gameTotalScore
   );
   console.log("Sorted games", sortedGames);
-  return <h1>Summary</h1>;
+  return (
+    <div className="summary">
+      <h1>Summary</h1>
+      {sortedGames.map((game) => (
+        <div key={game.id}>
+          <p>{game.teamHome.team} vs</p>
+          <p>{game.teamAway.team}</p>
+          <p></p>
+          <p>{game.teamHome.score}</p>
+          <p>{game.teamAway.score}</p>
+          <h4>Total: {game.gameTotalScore}</h4>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Summary;
